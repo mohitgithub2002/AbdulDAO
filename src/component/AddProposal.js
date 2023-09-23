dabf67c14f3dc9360aaf45e3f89069fd7538088a
Mohit Goyal <mohit.work2002@gmail.com>
2023-09-23 17:15:14 +0530

    Add: multiple options feature

--- src/component/AddProposal.js
+++ src/component/AddProposal.js
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import {contract} from "../connectContract"
-
+import { BsPlusCircle } from "react-icons/bs";
 const AddProposal = () => {
   const [topic,setTopic] = useState();
   const [startTime,setStartTime] = useState();
   const [endTime,setEndTime] = useState();
   const [question,setQuestion] = useState();
   const [options,setOptions] = useState(["",""]);
+  const addOption = () => {
+    const newOptions = [...options];
+    newOptions.push("");
+    setOptions(newOptions);
+  }
   const handleOptionChange = (index, value) => {
     const newOptions = [...options];
     newOptions[index] = value;
@@ -34,7 +39,7 @@ const AddProposal = () => {
       <div class="relative min-h-screen  w-full ">
         <div class="absolute inset-0 z-0 h-full w-full "></div>
         <div class="container mx-auto  ">
-          <div class="flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md absolute top-2/4 left-2/4  w-full sm:max-w-[32rem] max-w-[20rem] -translate-y-2/4 -translate-x-2/4">
+          <div class=" overflow-scroll flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 shadow-md absolute top-2/4 left-2/4  w-full sm:max-w-[32rem] max-w-[20rem] -translate-y-2/4 -translate-x-2/4">
             <div class="relative bg-clip-border mx-4 rounded-xl overflow-hidden bg-gradient-to-tr from-blue-600 to-blue-400 text-white shadow-blue-500/40 shadow-lg -mt-6 mb-4 grid h-28 place-items-center">
               <h3 class="block antialiased tracking-normal font-sans text-3xl font-semibold leading-snug text-white">
                 Add New Proposal (OnlyOwner)
@@ -85,7 +90,8 @@ const AddProposal = () => {
                   Question
                 </label>
               </div>
-               {options.map((option,index)=>(
+              <div className="overflow-y-auto max-h-40">
+                {options.map((option,index)=>(
                   <div class="relative w-full min-w-[200px] h-16">
                   <input
                     type="text"
@@ -97,19 +103,17 @@ const AddProposal = () => {
                     Options {index+1}
                   </label>
                 </div>
-               ))} 
-              
-
-              {/* <div class="relative w-full min-w-[200px] h-16">
-                <input
-                  type="text"
-                  class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-3 rounded-md border-blue-gray-200 focus:border-blue-500"
-                  placeholder=" "
-                />
-                <label class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-[18px] text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[4.1] text-blue-gray-400 peer-focus:text-blue-500 before:border-blue-gray-200 peer-focus:before:border-blue-500 after:border-blue-gray-200 peer-focus:after:border-blue-500">
-                  Options B
-                </label>
-              </div> */}
+               ))}
+              </div>
+               
+               <button
+                  type="button"
+                  className="flex items-center text-blue-500 hover:text-blue-700 cursor-pointer"
+                  onClick={addOption}
+                >
+                  <BsPlusCircle className="w-6 h-6 mr-1" /> Add Option
+                </button> 
+                              
             </div>
             <div class="p-6 pt-0">
               <button
--- src/component/EditProposal.js
+++ src/component/EditProposal.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import {contract} from "../connectContract"
 import { useParams } from 'react-router-dom';
+import { BsPlusCircle } from "react-icons/bs";
 const EditProposal = () => {
     const { id } = useParams();
   const [topic,setTopic] = useState();
@@ -8,6 +9,11 @@ const EditProposal = () => {
   const [endTime,setEndTime] = useState();
   const [question,setQuestion] = useState();
   const [options,setOptions] = useState(["",""]);
+  const addOption = () => {
+    const newOptions = [...options];
+    newOptions.push("");
+    setOptions(newOptions);
+  };
   const handleOptionChange = (index, value) => {
     const newOptions = [...options];
     newOptions[index] = value;
@@ -109,21 +115,29 @@ const EditProposal = () => {
                   Question
                 </label>
               </div>
-               {options.map((option,index)=>(
+              <div className="overflow-y-auto max-h-40">
+                {options.map((option,index)=>(
                   <div class="relative w-full min-w-[200px] h-16">
                   <input
                     type="text"
                     class="peer w-full h-full bg-transparent text-blue-gray-700 font-sans font-normal outline outline-0 focus:outline-0 disabled:bg-blue-gray-50 disabled:border-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 border focus:border-2 border-t-transparent focus:border-t-transparent text-sm px-3 py-3 rounded-md border-blue-gray-200 focus:border-blue-500"
                     placeholder=" "
-                    value={options[index]}
+                    value={option}
                     onChange={(e)=>handleOptionChange(index,e.target.value)}
                   />
                   <label class="flex w-full h-full select-none pointer-events-none absolute left-0 font-normal peer-placeholder-shown:text-blue-gray-500 leading-tight peer-focus:leading-tight peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500 transition-all -top-1.5 peer-placeholder-shown:text-[18px] text-[11px] peer-focus:text-[11px] before:content[' '] before:block before:box-border before:w-2.5 before:h-1.5 before:mt-[6.5px] before:mr-1 peer-placeholder-shown:before:border-transparent before:rounded-tl-md before:border-t peer-focus:before:border-t-2 before:border-l peer-focus:before:border-l-2 before:pointer-events-none before:transition-all peer-disabled:before:border-transparent after:content[' '] after:block after:flex-grow after:box-border after:w-2.5 after:h-1.5 after:mt-[6.5px] after:ml-1 peer-placeholder-shown:after:border-transparent after:rounded-tr-md after:border-t peer-focus:after:border-t-2 after:border-r peer-focus:after:border-r-2 after:pointer-events-none after:transition-all peer-disabled:after:border-transparent peer-placeholder-shown:leading-[4.1] text-blue-gray-400 peer-focus:text-blue-500 before:border-blue-gray-200 peer-focus:before:border-blue-500 after:border-blue-gray-200 peer-focus:after:border-blue-500">
                     Options {index+1}
                   </label>
                 </div>
-               ))} 
-              
+               ))}
+              </div> 
+              <button
+                  type="button"
+                  className="flex items-center text-blue-500 hover:text-blue-700 cursor-pointer"
+                  onClick={addOption}
+                >
+                  <BsPlusCircle className="w-6 h-6 mr-1" /> Add Option
+                </button>
 
               {/* <div class="relative w-full min-w-[200px] h-16">
                 <input
